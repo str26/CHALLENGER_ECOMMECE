af8124b9d54a1d5deab82a644b2c105bbdbfcf13
str26 <estherj_94@hotmail.com>
2023-07-16 20:47:26 -0500

    se actualiza las rutas por cambio de directorio  y se agrega componente listaClientes para admon de inicio de sesion

---
+++ src/app.js
@@ -0,0 +1,19 @@
+
+// import './App.css';
+// import { Cabecera } from './componentes/Cabecera';
+// import { BrowserRouter } from 'react-router-dom';
+// import { Ruteo } from './rutas/Ruteo';
+
+// function App() {
+//   return (
+//     <div className="container-fluid">
+//       <BrowserRouter>
+//         <Cabecera />
+//         <Ruteo/>
+//       </BrowserRouter>
+
+//     </div>
+//   );
+// }
+
+// export default App;
